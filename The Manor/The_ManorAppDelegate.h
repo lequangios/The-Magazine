6ba3d0975573d2131c5fe2879682379ce93db7d9
Dung <Dung@LeDungs-iMac.local>
2013-09-26 11:10:27 +0700

    Initial Commit

---
+++ The Manor/The_ManorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  The_ManorAppDelegate.h
+//  The Manor
+//
+//  Created by Dung on 9/26/13.
+//  Copyright (c) 2013 The Manor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface The_ManorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ The Manor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  The Manor
+//
+//  Created by Dung on 9/26/13.
+//  Copyright (c) 2013 The Manor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "The_ManorAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([The_ManorAppDelegate class]));
+    }
+}
